164c07c3f56863f978c538000e43fdfed98e0af9
saranshkhulbe7 <saranshkhulbe7@gmail.com>
2023-03-26 18:46:15 +0530

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import {DocumentMagnifyingGlassIcon} from "@heroicons/react/24/outline"
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+        <div>
+            <DocumentMagnifyingGlassIcon className="h-64 w-64 text-indigo-600/20" />
+        </div>
+    </div>
+  )
+}
+
+export default Home
